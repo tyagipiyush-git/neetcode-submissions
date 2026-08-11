91e2ab97687da774e396d934183ad4bdddceaa76
Piyush Tyagi <piyush.del.tyagi@gmail.com>
2026-08-11 22:56:15 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,14 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = (re.sub(r"[^a-zA-Z0-9]", "", s)).lower()
+        left = 0
+        right = len(cleaned)-1
+
+        while left < right:
+            if cleaned[left] != cleaned[right]:
+                return False
+            left +=1
+            right -=1
+
+        return True
